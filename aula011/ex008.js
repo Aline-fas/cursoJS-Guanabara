84922632de666edc502233e34deeff88151dd473
ALINE FERNANDES AMARO <alinefas2020@gmail.com>
2022-10-02 11:35:48 -0300

    estudos de JavaScript

---
+++ aula011/ex008.js
@@ -0,0 +1,11 @@
+var velocidade = 50
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+
+if (velocidade > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+
+} else {
+    console.log(`Parabens`)
+}
+
+console.log(`Dirija csempre usando cinto de segurança!`)
